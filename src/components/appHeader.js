4795a2fdf5000059eab077bd3d349b2f510d4839
Bischoff, Silas <SBischoff@brockhaus-ag.de>
2019-02-06 02:29:34 +0100

    Add Navbar to header

--- src/components/appHeader.js
+++ src/components/appHeader.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import { Button } from './button'
-import '../styles/App.css';
+import '../styles/appHeader.css';
 const logo = require('../ressources/logo_wheel.png');
 
 export class AppHeader extends Component {
     render() {
         return(
-        <header className="App-header">
-          <img src={logo} className="logo-wheel" alt="logo-wheel" />
-          <p>
-            We at <b>FEELING CONFIDENCE</b> are working on an awesome website for our coaching programme.
-          </p>
-          <Button text="Register for Email"/>
-        </header>
+            <header className="App-header">
+                <div className="Home-content">
+                    <img src={logo} className="Logo-wheel" alt="logo-wheel" />
+                    <p>
+                        We at <b>FEELING CONFIDENCE</b> are working on an awesome website for our coaching programme.
+                    </p>
+                    <Button text="Register for Email"/>
+                </div>
+                <div className="Navbar">
+                    <a class="active" href="#home">Home</a>
+                    <a href="#programme">The Programme</a>
+                    <a href="#aboutus">About Us</a>
+                    <a href="#impressum">Impressum</a>
+                </div>
+            </header>
         );
     }
 }
